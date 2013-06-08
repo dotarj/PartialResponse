d15312ba4888488be6779aebaedb4e2b8134a3ce
Arjen Post <mail@arjenpost.nl>
2013-06-08 20:29:05 +0200

    Release 0.1 AssemblyInfo update

--- src/PartialResponse/Properties/AssemblyInfo.cs
+++ src/PartialResponse/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PartialResponse")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Partial Response provides partial response (a.k.a. fields) support for ASP.NET Web API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arjen Post")]
 [assembly: AssemblyProduct("PartialResponse")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
