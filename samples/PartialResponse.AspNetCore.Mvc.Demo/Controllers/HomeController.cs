5175021dfa52fa00fe816e1478fc34ae604bf8cc
Arjen Post <mail@arjenpost.nl>
2017-02-10 14:42:32 +0100

    sample project configured

--- samples/PartialResponse.AspNetCore.Mvc.Demo/Controllers/HomeController.cs
+++ samples/PartialResponse.AspNetCore.Mvc.Demo/Controllers/HomeController.cs
@@ -1,12 +1,44 @@
+using System.Collections.Generic ;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public List<dynamic> Index()
         {
-            return Json("");
+            var response = new List<dynamic>()
+            {
+                new
+                {
+                    Foo = 1,
+                    Bar = new
+                    {
+                        Baz = 2,
+                        Qux = 3
+                    }
+                },
+                new
+                {
+                    Foo = 2,
+                    Bar = new
+                    {
+                        Baz = 3,
+                        Qux = 4
+                    }
+                },
+                new
+                {
+                    Foo = 3,
+                    Bar = new
+                    {
+                        Baz = 5,
+                        Qux = 6
+                    }
+                }
+            };
+
+            return response;
         }
     }
 }
--- samples/PartialResponse.AspNetCore.Mvc.Demo/Startup.cs
+++ samples/PartialResponse.AspNetCore.Mvc.Demo/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PartialResponse.Extensions.DependencyInjection;
 
 namespace WebApplication
 {
@@ -24,7 +25,9 @@ namespace WebApplication
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services
+                .AddMvc(options => options.OutputFormatters.Clear())
+                .AddPartialJsonFormatters();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
