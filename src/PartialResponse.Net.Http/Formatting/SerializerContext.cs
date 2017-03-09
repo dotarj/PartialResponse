de9010dea2e1f05dce424e17e5c4aef063e3ca45
Arjen Post <mail@arjenpost.nl>
2017-03-09 10:09:38 +0100

    json serializer extensions copied to web api implementation

---
+++ src/PartialResponse.Net.Http/Formatting/SerializerContext.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Arjen Post. See License.txt and Notice.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace PartialResponse.Net.Http.Formatting
+{
+    internal class SerializerContext
+    {
+        private readonly Func<string, bool> shouldSerialize;
+        private readonly Dictionary<string, bool> cache = new Dictionary<string, bool>();
+
+        public SerializerContext(Func<string, bool> shouldSerialize)
+        {
+            this.shouldSerialize = shouldSerialize;
+        }
+
+        public bool ShouldSerialize(string path)
+        {
+            if (this.cache.ContainsKey(path))
+            {
+                return this.cache[path];
+            }
+
+            var result = !this.shouldSerialize(path);
+
+            this.cache.Add(path, result);
+
+            return result;
+        }
+    }
+}
